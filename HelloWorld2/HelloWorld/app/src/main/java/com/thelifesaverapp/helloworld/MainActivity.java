722954d5a0be529a7ef8bb2c4b913fa4ec867eac
Frys28 <okeychris0@hotmail.com>
2018-09-14 20:48:19 -0700

    Thanking God

---
+++ HelloWorld2/HelloWorld/app/src/main/java/com/thelifesaverapp/helloworld/MainActivity.java
@@ -0,0 +1,26 @@
+package com.thelifesaverapp.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.util.Log.i;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/thelifesaverapp/helloworld/MainActivity.java
@@ -0,0 +1,25 @@
+package com.thelifesaverapp.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((TextView) findViewById(R.id.textView)).setTextColor(
+                        getResources().getColor(R.color.colorPrimary));
+            }
+        });
+        //I;m excited for this class
+        //System.out.println("Hello World");
+    }
+}
